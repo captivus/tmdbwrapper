f13ce8ef44a19b6ce4feb29aff143cdd2262b03c
captivus <366332+captivus@users.noreply.github.com>
2022-09-08 10:55:15 -0500

    initial commit

---
+++ tmdbwrapper/__init__.py
@@ -0,0 +1,23 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+TMDB_API_KEY = os.environ.get('TMDB_API_KEY', None)
+
+class APIKeyMissingError(Exception):
+    pass
+
+if TMDB_API_KEY is None:
+    raise APIKeyMissingError(
+        "All methods require an API key.  See "
+        "https://developers.themoviedb.org/3/getting-started/introduction "
+        "for how to retrieve an authentication token from "
+        "The Movie Database"
+    )
+
+session = requests.Session()
+session.params = {}
+session.params['api_key'] = TMDB_API_KEY
+
+from .tv import TV
